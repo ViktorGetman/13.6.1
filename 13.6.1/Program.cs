a044c65ef4749248827ec984c571de51cdca9488
ViktorGetman <123113442+ViktorGetman@users.noreply.github.com>
2023-04-27 21:35:46 +0300

    Добавьте файлы проекта.

---
+++ 13.6.1/Program.cs
@@ -0,0 +1,53 @@
+﻿using System.Diagnostics;
+
+namespace PhoneBook
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Укажите ссылку на ваш текстовый файл");
+            var fileLink=Console.ReadLine();
+            
+            var list = new List<string>();
+            var list2 = new LinkedList<string>();
+            string text;
+            StreamReader sr = new StreamReader(fileLink);
+
+
+            var watchTwo = Stopwatch.StartNew();
+
+            text = sr.ReadLine();
+            while (text != null)
+            {
+                list.Add(text);
+                //Read the next line
+                text = sr.ReadLine();
+            }
+
+            watchTwo.Stop();
+            sr.Close();
+            Console.WriteLine(list.Count);
+
+            Console.WriteLine($"Вставка в список List: {watchTwo.Elapsed.TotalMilliseconds}  мс");
+
+            StreamReader sr2 = new StreamReader(fileLink);
+            var watchTwo2 = Stopwatch.StartNew();
+
+
+            text = sr2.ReadLine();
+            while (text != null)
+            {
+                list2.AddFirst(text);
+
+                text = sr2.ReadLine();
+            }
+
+            watchTwo2.Stop();
+            sr2.Close();
+            Console.WriteLine(list2.Count);
+
+            Console.WriteLine($"Вставка в список LinkedList: {watchTwo2.Elapsed.TotalMilliseconds}  мс");
+        }
+    }
+}
